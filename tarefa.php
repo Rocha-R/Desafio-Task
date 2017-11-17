b85f94c0cea906b1e31cdb4e4ff4ffc465f63b6b
Rocha-R <33747868+Rocha-R@users.noreply.github.com>
2017-11-17 07:57:46 -0200

    Update tarefa.php

--- tarefa.php
+++ tarefa.php
@@ -10,31 +10,23 @@
 
 	   <?php
 	   
-	       if (isset($_GET['nome']) && $_GET['nome'] != '') {//Função isset para verificar se o indice foi definido,
-		   //no caso se o indice "nome" existe dentro do Array "$_GET" e
-		   //se o indice é diferente de vazio.
-		         //$_GET : Variável superGlobais (existem dentro do PHP já)
-				 
+	       if (isset($_GET['nome']) && $_GET['nome'] != '') {
 			$tarefa = array();
 
                 $tarefa['nome'] = $_GET['nome'];
 
 				 
-		     gravar_tarefa($conexao, $tarefa);/*A função gravar_tarefa()
-			 receberá a conexão que já temos, na variável $conexao, 
-			 e também a nossa tarefa, que está no array $tarefa.*/
-			 
+		     gravar_tarefa($conexao, $tarefa);
+		       
 			 header('Location: tarefa.php');
-			 die();//mata a operação de adição, assim logo após um cadastro
-			 //no banco de dados, podemos atualizar a página sem que a mesma
-			 //cadastre de novo a ultima tarefa.
+			 die();
 		}		
 		
 		$lista_tarefas = buscar_tarefas($conexao);	 
 						
 		//Este array serve para que o formulario.php sempre tenha uma tarefa
         //para exibir, evitando erros da variável $tarefa. Isto acotence
-        //pois decidimos manter o mesmo formulario fazendo 2 tarefas.
+        //pois o formulario existe para 2 tarefas.
 
         //A de editar (trazendo os resultados no campo).
 
@@ -47,10 +39,8 @@
 				 
 		
             			 
-			include "template.php";//Instrução que adiciona o arquivo 
-// em questão e todas as variáveis e funções nele criadas. 
-
-//Podendo ser utilizadas no arquivo que esta utilizando o include.			
+			include "template.php";
+		
 	      
 	   ?>
 	   
